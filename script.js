1fd1a54a9950003f9ae633556266668a02badfa8
Lamer-ON <lamer_on@mail.ru>
2020-10-02 10:22:53 +0300

    2 day start

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    let btn = document.querySelector('.button'),
+    input = document.querySelector('.input');
+
+btn.addEventListener('click', function (e) {
+    input.innerHTML = '';}
+
+
+
+
+
+
+
+
+});
