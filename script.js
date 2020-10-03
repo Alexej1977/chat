9ed6de6b91f695210e349085ea0502bfc25ca8de
Your NameLamer-ON <lamer_on@mail.ru>
2020-10-03 22:46:15 +0300

    ok vlad without settimeout

--- script.js
+++ script.js
@@ -2,18 +2,40 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    let btn = document.querySelector('.button'),
-    input = document.querySelector('.input'),
-    text1 = document.querySelector('.area-1');
+	let i = 0,
+		btn = document.querySelector('.button'),
+		input = document.querySelector('.input'),
+		text1 = document.querySelector('.area-1'),
+		text2 = document.querySelector('.area-2');
 
-    btn.addEventListener('click', function (e) {
-    input.value = '';
-    text1.value = '';
-    });
+	btn.addEventListener('click', function (e) {
 
-    input.addEventListener('input', function(e){
-         text1.value = input.value;   
-    })
+		if (text1.value !== '') {
+			i += 1;
+			text2.innerHTML += `<div class="area-2__wrapper">
+			<span class="close">&times </span><span class="area-2__text">${input.value}</span></div> `;
+
+			if (i == 11) {
+				text2.firstElementChild.remove();
+				--i;
+			}
+
+			let close = document.querySelectorAll('.close');
+			close.forEach(e => {
+				e.addEventListener('click', function (e) {
+					e.preventDefault();
+					console.log('close');
+					e.target.parentElement.remove();
+					--i;
+				});
+			});
+			input.value = '';
+			text1.value = '';
+		}
+	});
+
+	input.addEventListener('input', function (e) {
+		text1.value = input.value;
+	})
 
 });
-
